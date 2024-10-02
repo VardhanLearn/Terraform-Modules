bd98caba584324d119d8ba89882224db90629ae1
VardhanLearn <87961252+VardhanLearn@users.noreply.github.com>
2024-10-02 20:58:38 +0530

    Update backend.tf

--- Dev/backend.tf
+++ Dev/backend.tf
@@ -3,7 +3,5 @@ terraform {
     bucket = "DevopsB4"
     key    = "modules-Devopsb4.tfstate"
     region = "us-east-1"
-    # dynamodb_table = "devopsb27-terraform-locking"
-    # encrypt        = true
   }
 }
